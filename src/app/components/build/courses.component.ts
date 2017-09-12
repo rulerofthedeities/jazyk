5ebe85fa51683e09dd184fdfbad3027f0b21e1bc
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2017-09-12 11:14:36 +0200

    Built courses are grouped per language

--- src/app/components/build/courses.component.ts
+++ src/app/components/build/courses.component.ts
@@ -7,30 +7,24 @@ import {UserService} from '../../services/user.service';
 import {Course, Language, Translation} from '../../models/course.model';
 import 'rxjs/add/operator/takeWhile';
 
-@Component({
-  template: `
-  <button class="btn btn-success" (click)="onNewCourse()">
-    {{text["newcourse"]}}
-  </button>
+interface Map<T> {
+  [K: string]: T;
+}
 
-  <!-- COURSE LIST -->
-  <ul class="list-unstyled" *ngIf="courses">
-    <li *ngFor="let course of courses">
-      <km-build-course-summary
-        [course]="course"
-        [text]="text">
-      </km-build-course-summary>
-    </li>
-  </ul>
-  <km-info-msg [msg]="infoMsg" *ngIf="infoMsg">
-  </km-info-msg>`
+@Component({
+  templateUrl: 'courses.component.html',
+  styleUrls: ['../learn/courses.component.css']
 })
 
 export class BuildCoursesComponent implements OnInit, OnDestroy {
   private componentActive = true;
+  private lanCourses: Map<Course[]> = {}; // Group courses per language
   courses: Course[];
   text: Object = {};
+  selectedLanguage: Language;
+  languages: Language[];
   infoMsg: string;
+  multipleLanguages = false;
 
   constructor(
     private router: Router,
@@ -46,6 +40,11 @@ export class BuildCoursesComponent implements OnInit, OnDestroy {
 
   onNewCourse() {
     this.router.navigate(['/build/course/new']);
+  }
+
+  onLanguageSelected(newLanguage: Language) {
+    this.selectedLanguage = newLanguage;
+    this.filterCourses();
   }
 
   private getTranslations() {
@@ -71,10 +70,44 @@ export class BuildCoursesComponent implements OnInit, OnDestroy {
         this.courses = courses;
         if (courses && courses.length < 1) {
           this.infoMsg = this.text['NoBuiltCourses'];
+        } else {
+          this.getLanguages();
         }
       },
       error => this.errorService.handleError(error)
     );
+  }
+  private getLanguages() {
+    const AllLanguages = this.utilsService.getActiveLanguages();
+    const languages: Language[] = [];
+    let courseLan;
+    AllLanguages.filter( language => {
+      courseLan = this.courses.filter( course => course.languagePair.to === language._id);
+      if (courseLan.length > 0) {
+        languages.push(language);
+        this.lanCourses[language._id] = courseLan;
+      }
+    });
+    this.languages = languages;
+    this.selectedLanguage = this.userService.getUserLearnLanguage(this.languages);
+    this.filterCourses();
+    if (this.languages.length > 1) {
+      this.multipleLanguages = true;
+    }
+  }
+
+  private filterCourses() {
+    let lan;
+    if (this.selectedLanguage) {
+      lan = this.selectedLanguage._id;
+    } else {
+      lan = this.utilsService.getDefaultLanguage();
+    }
+    if (this.lanCourses[lan]) {
+      this.courses = this.lanCourses[lan];
+    } else {
+      this.courses = null;
+    }
   }
 
   ngOnDestroy() {
