edf319b5eda6ba1c6031fe5955ae8e2ff0f1c289
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2017-10-07 11:53:01 +0200

    Show synonyms in exercise questions

--- src/app/components/learn/question.component.ts
+++ src/app/components/learn/question.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {ExerciseData} from '../../models/exercise.model';
+import {ExerciseData, Exercise} from '../../models/exercise.model';
 import {LearnSettings} from '../../models/user.model';
 import {LanPair} from '../../models/course.model';
 
@@ -15,4 +15,15 @@ export class LearnQuestionComponent {
   @Input() currentData: ExerciseData;
   @Input() dir: string;
   @Input() settings: LearnSettings = null;
+
+  getLocalAlts(word: Exercise): string {
+    let altwords = '';
+    console.log('local', word);
+    if (word && word.local && word.local.alt) {
+    console.log('local', word.local.alt);
+      altwords = word.local.alt.split('|').join(', ');
+    }
+    console.log('local', altwords);
+    return altwords;
+  }
 }
