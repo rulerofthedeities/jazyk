6e3318ca6ba9fe62e75f90c6d959f63673790927
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2018-06-06 19:56:41 +0200

    Display chapter name on lesson header in read mode

--- src/app/components/build/lesson.component.ts
+++ src/app/components/build/lesson.component.ts
@@ -4,7 +4,8 @@ import {BuildService} from '../../services/build.service';
 import {UtilsService} from '../../services/utils.service';
 import {ErrorService} from '../../services/error.service';
 import {UserService} from '../../services/user.service';
-import {Course, Lesson, Translation, LanPair, LanConfigs, AccessLevel} from '../../models/course.model';
+import {Course, Lesson, Translation, LanPair,
+        LanConfigs, AccessLevel, LessonId} from '../../models/course.model';
 import {Filter, WordPairDetail} from '../../models/word.model';
 import {Exercise, ExerciseType} from '../../models/exercise.model';
 import {takeWhile, filter} from 'rxjs/operators';
