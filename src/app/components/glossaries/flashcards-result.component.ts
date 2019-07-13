b23290a11c66a7365a36a3ec46cc7b1a13ca97a9
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2019-07-13 20:15:45 +0200

    Added rank promotion to flashcards

--- src/app/components/glossaries/flashcards-result.component.ts
+++ src/app/components/glossaries/flashcards-result.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { UserService } from 'app/services/user.service';
+import { SharedService } from 'app/services/shared.service';
 import { Map, LanPair } from 'app/models/main.model';
 import { FlashCardResult, AnswerData } from 'app/models/word.model';
+import { ModalPromotionComponent } from '../modals/modal-promotion.component';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'km-flashcards-result',
@@ -8,13 +12,22 @@ import { FlashCardResult, AnswerData } from 'app/models/word.model';
   styleUrls: ['flashcards-result.component.css']
 })
 
-export class BookFlashCardsResultComponent implements OnInit {
+export class BookFlashCardsResultComponent implements OnInit, OnDestroy {
   @Input() text: Object;
   @Input() lanPair: LanPair;
   @Input() audioPath: string;
   @Input() answers: Map<AnswerData>;
   @Input() flashCards: FlashCardResult[];
+  @ViewChild(ModalPromotionComponent) promotionComponent: ModalPromotionComponent;
+  private componentActive = true;
   totalPoints: number;
+  rankKey: string;
+  rankNr: number;
+
+  constructor(
+    private userService: UserService,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit() {
     this.totalPoints = 0;
@@ -23,5 +36,40 @@ export class BookFlashCardsResultComponent implements OnInit {
       flashCard.points = this.answers[flashCard.wordId].points;
       this.totalPoints += flashCard.points;
     });
+    this.checkNewRank();
+  }
+
+  getGender(): string {
+    return this.userService.user.main.gender || 'm';
+  }
+
+  private checkNewRank() {
+    this.userService
+    .fetchScoreTotal(null)
+    .pipe(takeWhile(() => this.componentActive))
+    .subscribe(
+      score => {
+        const scoreTotal = score || 0,
+        rank = this.sharedService.getRank(scoreTotal),
+        previousRank = this.sharedService.getRank(scoreTotal - this.totalPoints);
+        if (rank > previousRank) {
+          this.newRankPromotion(rank);
+        }
+        this.sharedService.onScoreChanged(scoreTotal);
+      }
+    );
+  }
+
+  private newRankPromotion(newRank: number) {
+    if (this.promotionComponent) {
+      // Show promotion modal
+      this.rankNr = newRank || 0;
+      this.rankKey = 'rank' + (this.rankNr).toString() + this.userService.user.main.gender || 'm';
+      this.promotionComponent.doShowModal();
+    }
+  }
+
+  ngOnDestroy() {
+    this.componentActive = false;
   }
 }
